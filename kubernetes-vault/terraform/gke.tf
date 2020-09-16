a8bd00418b62af49c5db06201ace657cac950bad
Gleb Stepanov <gleb.stepanov@nordigy.ru>
2020-09-16 09:37:46 +0300

    boom

---
+++ kubernetes-vault/terraform/gke.tf
@@ -0,0 +1,54 @@
+resource "google_project_service" "kubernetes" {
+  service = "container.googleapis.com"
+}
+
+resource "google_container_cluster" "kubernetes" {
+  name               = "otus-k8s-cluster"
+  location           = "europe-west1"
+  min_master_version = "1.16.13-gke.1"
+  monitoring_service = "none"
+  # stackdriver disabled
+  logging_service = "none"
+
+  node_locations = [
+    "europe-west1-d",
+  ]
+
+  remove_default_node_pool = true
+  initial_node_count = 3
+
+  master_auth {
+    username = ""
+    password = ""
+  }
+
+
+}
+
+resource "google_container_node_pool" "default-pool" {
+  name               = "default-pool"
+  location           = google_container_cluster.kubernetes.location
+  cluster            = google_container_cluster.kubernetes.name
+  initial_node_count = 3
+  autoscaling {
+    min_node_count = 3
+    max_node_count = 5
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/monitoring.write"
+    ]
+  }
+}
+
+
+
